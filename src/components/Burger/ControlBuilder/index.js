a9830af31b2483ed22f5c07c51d0e193048f4110
Khanh Dao <khanh_dao@epam.com>
2020-07-15 00:00:59 +0700

    Implement add and remove ingredient and price total

--- src/components/Burger/ControlBuilder/index.js
+++ src/components/Burger/ControlBuilder/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Controller from './Controllers';
+import classes from './ControlBuilder.css';
 
 const controls = [
   {label: 'Salad', type: 'salad'},
@@ -10,15 +11,18 @@ const controls = [
 
 const ControlBuilder = (props) => {
   return (
-    <div>
-      {controls && controls.map(item => 
-        <Controller 
-          key={item.label} 
-          label={item.label}
-          onAdd={() => props.addIngredient(item.type)}
-          onRemove= {() => props.removeIngredient(item.type)}
-        />
-      )}
+    <div className={classes.Wrapper}>
+      <div>
+        {controls && controls.map(item => 
+          <Controller 
+            key={item.label} 
+            label={item.label}
+            onAdd={() => props.addIngredient(item.type)}
+            onRemove= {() => props.removeIngredient(item.type)}
+          />
+        )}
+      </div>
+        <div className={classes.TotalPrice}> {`Total Price: ${props.totalPrice}$`}</div>
     </div>
   )
 }
--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -6,39 +6,63 @@ class BurgerBuilder extends React.Component {
   constructor(props){
     super(props)
 
-    this.ingredients = {
-      cheese: 2,
-      meat: 0,
-      salad: 1,
-      bacon: 1,
+    this.state = {
+      ingredients : {
+        cheese: 0,
+        meat: 0,
+        salad: 0,
+        bacon: 0,
+      },
+      pricePerUnit: {
+        cheese: 0.5,
+        meat: 1.5,
+        salad: 0.5,
+        bacon: 1,
+      },
+      totalPrice: 0,
     }
   }
 
   onAddIngredients = (type) => {
-    let newIngre = {...this.ingredients};
+
+    let newIngre = {...this.state.ingredients};
     newIngre[type] += 1;
-    Object.assign(this.ingredients, newIngre)
-    console.log(this.ingredients)
-    this.setState({...this.ingredients})
+    
+    let oldPrice = this.state.totalPrice;
+    let newPrice = oldPrice + this.state.pricePerUnit[type];
+    console.log(`Checking fomulas ${this.state.pricePerUnit[type]} x ${newIngre[type]} and total ${this.state.totalPrice}`);
+    this.setState( prevState => {
+      return {
+        ingredients: newIngre,
+        totalPrice: newPrice
+      }
+    });
   };
 
   onRemoveIngredient = (type) => {
-    if(this.ingredients[type] === 0) return;
-    let newIngre = {...this.ingredients};
+    if(this.state.ingredients[type] === 0) return;
+    let newIngre = {...this.state.ingredients};
     newIngre[type] -= 1;
-    Object.assign(this.ingredients, newIngre);
-    this.setState({...this.ingredients})
+
+    let oldPrice = this.state.totalPrice;
+    let newPrice = oldPrice - this.state.pricePerUnit[type];
+
+    this.setState({
+      ingredients: newIngre,
+      totalPrice: newPrice,
+    })
   };
 
   render(){
     return(
-      <>
-      <Burger ingredients={this.ingredients} />
-      <Controller 
-        addIngredient={this.onAddIngredients}
-        removeIngredient={this.onRemoveIngredient}
+      <div>
+        <Burger ingredients={this.state.ingredients} />
+        <Controller 
+          addIngredient={this.onAddIngredients}
+          removeIngredient={this.onRemoveIngredient}
+          totalPrice={this.state.totalPrice}
         />
-      </>
+      </div>
     );
   }
 };
